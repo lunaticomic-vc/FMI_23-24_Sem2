f0737710bb2edbb8f85211817f0446b8789381a9
Danichka <78638969+lunaticomic-vc@users.noreply.github.com>
2024-02-27 20:12:30 +0200

    Shte go opravqm na seminara

--- Sem 2/fileSizeFun.cpp
+++ Sem 2/fileSizeFun.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <cstring> 
+#include <cmath>
+#pragma warning(disable:4996)
+using std::cin;
+using std::cout;
+using std::endl;
+using std::swap;
 
+int fileSize
+
+int main()
+{
+}
