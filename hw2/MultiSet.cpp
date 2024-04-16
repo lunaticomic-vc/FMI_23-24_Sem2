334cfc9ef49a33666df89e931f00c4214887c65f
Danichka <78638969+lunaticomic-vc@users.noreply.github.com>
2024-04-16 12:55:16 +0300

    Create MultiSet.cpp

---
+++ hw2/MultiSet.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <fstream>
+
+class MultiSet {
+private:
+    unsigned char* counts; // Масив за съхранение на брой пъти, които всяко число се среща
+    int maxNumber; // Най-голямото число в множеството
+    int maxCount; // Максимален брой срещания на едно число (2^k - 1)
+
+public:
+    MultiSet(int n, int k) {
+        maxNumber = n;
+        maxCount = (1 << k) - 1;
+        counts = new unsigned char[n + 1]; // Алокираме памет за n+1 числа
+        std::fill(counts, counts + n + 1, 0); // Инициализация на масива с 0
+    }
+
+    ~MultiSet() {
+        delete[] counts;
+    }
+
+    void add(int number) 
+    {
+        if (number <= maxNumber && counts[number] < maxCount) 
+        {
+            counts[number]++;
+        }
+    }
+
+    int count(int number) const 
+    {
+        if (number > maxNumber) return 0;
+        return counts[number];
+    }
+
+    void print() const 
+    {
+        for (int i = 0; i <= maxNumber; ++i) 
+        {
+            if (counts[i] > 0) 
+            {
+                std::cout << i << " appears " << (int)counts[i] << " times\n";
+            }
+        }
+    }
+
+    void printRaw() const 
+    {
+        std::cout << "Memory representation:\n";
+      
+        for (int i = 0; i <= maxNumber; ++i) 
+        {
+            std::cout << (int)counts[i] << " ";
+        }
+        std::cout << "\n";
+    }
+
+    void serialize(const char* filename) const 
+    {
+        std::ofstream out(filename, std::ios::binary);
+      
+        if(out.is_open())
+        {
+          out.write((char*)counts, maxNumber + 1);
+          out.close();
+        }
+        else 
+            std::cout << "File issue"<< std::cout;
+    }
+
+    void deserialize(const char* filename) 
+    {
+        std::ifstream in(filename, std::ios::binary);
+
+       if(out.is_open())
+        {
+          in.read((char*)counts, maxNumber + 1);
+          in.close();
+        }
+        else 
+            std::cout << "File issue"<< std::cout;
+    }
+
+    MultiSet intersection(const MultiSet& other) const 
+    {
+        MultiSet result(maxNumber, std::min((int)log2(maxCount + 1), (int)log2(other.maxCount + 1)));
+      
+        for (int i = 0; i <= maxNumber; ++i) 
+        {
+            result.counts[i] = std::min(counts[i], other.counts[i]);
+        }
+      
+        return result;
+    }
+
+    MultiSet difference(const MultiSet& other) const 
+{
+        MultiSet result(maxNumber, std::min((int)log2(maxCount + 1), (int)log2(other.maxCount + 1)));
+  
+        for (int i = 0; i <= maxNumber; ++i) 
+        {
+            result.counts[i] = counts[i] > other.counts[i] ? counts[i] - other.counts[i] : 0;
+        }
+  
+        return result;
+    }
+
+    MultiSet complement() const 
+{
+        MultiSet result(maxNumber, std::min((int)log2(maxCount + 1), (int)log2(maxCount + 1)));
+  
+        for (int i = 0; i <= maxNumber; ++i) 
+        {
+            result.counts[i] = maxCount - counts[i];
+        }
+        return result;
+    }
+};
