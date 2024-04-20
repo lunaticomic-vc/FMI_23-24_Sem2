d59fd29bcf170783f79e412c9459674770ae0a18
Danichka <78638969+lunaticomic-vc@users.noreply.github.com>
2024-04-20 12:47:41 +0300

    Update MultiSet.cpp

--- hw2/MultiSet.cpp
+++ hw2/MultiSet.cpp
@@ -14,22 +14,27 @@ class Multiset {
         return (num * k) / BITS_IN_BYTE;
     }
 
-    int getBitOffset(unsigned int num) const {
+    int getBitOffset(unsigned int num) const 
+    {
         return (num * k) % BITS_IN_BYTE;
     }
 
-    unsigned char getOccurrence(unsigned int num) const {
-        if (num < 1 || num > n) {
+    unsigned char getOccurrence(unsigned int num) const 
+    {
+        if (num < 1 || num > n) 
+        {
             throw std::out_of_range("Number out of range");
         }
 
         int byteIndex = getByteIndex(num);
         int bitOffset = getBitOffset(num);
+
         unsigned int bitsNeeded = k;
         unsigned int result = 0;
 
-        while (bitsNeeded > 0) {
-            unsigned int bitsInThisByte = std::min(static_cast<unsigned int>(BITS_IN_BYTE - bitOffset), bitsNeeded);
+        while (bitsNeeded > 0) 
+        {
+            unsigned int bitsInThisByte = std::min((unsigned int)(BITS_IN_BYTE - bitOffset), bitsNeeded);
             unsigned int mask = (1 << bitsInThisByte) - 1;
             unsigned int bits = (set[byteIndex] >> bitOffset) & mask;
             result |= (bits << (k - bitsNeeded));
@@ -41,8 +46,10 @@ class Multiset {
         return result;
     }
 
-    void setOccurrence(unsigned int num, unsigned int count) {
-        if (num < 1 || num > n) {
+    void setOccurrence(unsigned int num, unsigned int count) 
+    {
+        if (num < 1 or num > n) 
+        {
             throw std::out_of_range("Number out of range");
         }
 
@@ -50,8 +57,9 @@ class Multiset {
         int bitOffset = getBitOffset(num);
         unsigned int bitsNeeded = k;
 
-        while (bitsNeeded > 0) {
-            unsigned int bitsInThisByte = std::min(static_cast<unsigned int>(BITS_IN_BYTE - bitOffset), bitsNeeded);
+        while (bitsNeeded > 0) 
+        {
+            unsigned int bitsInThisByte = std::min((unsigned int)(BITS_IN_BYTE - bitOffset), bitsNeeded);
             unsigned int mask = (1 << bitsInThisByte) - 1;
             set[byteIndex] &= ~(mask << bitOffset);
             set[byteIndex] |= ((count >> (k - bitsNeeded)) & mask) << bitOffset;
@@ -67,7 +75,7 @@ public:
         if (k < 1 || k > 8) {
             throw std::invalid_argument("k must be between 1 and 8");
         }
-        arraySize = ((n * k) + BITS_IN_BYTE - 1) / BITS_IN_BYTE;
+        arraySize = ((n * k) + BITS_IN_BYTE) / BITS_IN_BYTE;
         set = new unsigned char[arraySize]();
     }
 
@@ -75,59 +83,64 @@ public:
         delete[] set;
     }
 
-    void addNumber(unsigned int num) {
+    void addNumber(unsigned int num) 
+    {
         unsigned int current = getOccurrence(num);
-        if (current < ((1 << k) - 1)) {
+
+        if (current < ((1 << k) - 1)) 
+        {
             setOccurrence(num, current + 1);
         }
-        else {
+        else 
+        {
             throw std::overflow_error("Maximum occurrences reached");
         }
     }
 
-    unsigned char howManyOcc(unsigned int num) const {
+    unsigned char howManyOcc(unsigned int num) const 
+    {
         return getOccurrence(num);
     }
 
-    void serializeToBin(const std::string& fileName) const {
+    void serializeToBin(const std::string& fileName) const 
+    {
         std::ofstream file(fileName, std::ios::binary);
+
         file.write((char*)&n, sizeof(n));
         file.write((char*)&k, sizeof(k));
         file.write((char*)set, arraySize);
     }
 
-    void deserializeFromBin(const std::string& fileName) {
+    void deserializeFromBin(const std::string& fileName) 
+    {
         std::ifstream file(fileName, std::ios::binary);
+
         file.read((char*)&n, sizeof(n));
         file.read((char*)&k, sizeof(k));
+
         delete[] set;
-        arraySize = ((n * k) + BITS_IN_BYTE - 1) / BITS_IN_BYTE;
+
+        arraySize = ((n * k) + BITS_IN_BYTE) / BITS_IN_BYTE;
         set = new unsigned char[arraySize]();
         file.read((char*)set, arraySize);
     }
 
-    void printMultiset() const {
+    void printMultiset() const 
+    {
         for (unsigned int i = 1; i <= n; ++i) {
-            std::cout << i << ": " << static_cast<int>(howManyOcc(i)) << std::endl;
+            std::cout << i << ": " << (int)(howManyOcc(i)) << std::endl;
         }
     }
 };
 
 int main() {
     try {
-        Multiset ms(10, 4);  // max 10 numbers, using 4 bits each
+        Multiset ms(10, 4);
         ms.addNumber(1);
         ms.addNumber(1);
         ms.addNumber(1);
         ms.addNumber(1);
         ms.addNumber(1);
-        ms.addNumber(1);
-        ms.addNumber(1);
-        ms.addNumber(1);
-        ms.addNumber(1);
-        ms.addNumber(1);
-        ms.addNumber(3);
-        ms.addNumber(3);
         ms.addNumber(3);
         ms.addNumber(3);
         ms.addNumber(3);
@@ -135,7 +148,7 @@ int main() {
         ms.addNumber(3);
         ms.printMultiset();
         ms.serializeToBin("multiset.bin");
-
+        std::cout << std::endl;
         Multiset ms2(10, 4);
         ms2.deserializeFromBin("multiset.bin");
         ms2.printMultiset();
